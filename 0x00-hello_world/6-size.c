104e310a09035cc7593776afe7ca18c29fc1f037
temmydhayor <ttemitopealao@gmail.com>
2023-09-07 19:32:00 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - this describes the main function of thee code
- * Return: Always 0(success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
