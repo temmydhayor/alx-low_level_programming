898f966b6d30e2ca99bcbd48d9c3100dce414332
temmydhayor <ttemitopealao@gmail.com>
2023-09-09 16:45:43 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+ #include <stdio.h>
+
+/**
+ * main - this is the function of the task
+ * Return: 0
+ */
+
+int main(void)
+
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+
+}
