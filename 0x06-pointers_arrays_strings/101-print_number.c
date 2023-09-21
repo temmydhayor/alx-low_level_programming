03a6e9c74021ac1af08afa978eeb96b4ef80befd
temmydhayor <ttemitopealao@gmail.com>
2023-09-21 01:26:48 -0700

    101-print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,7 +2,7 @@
 
 /**
  * print_number - print numbers chars
- * @n: integer params
+ * @n: integer parameter
  * Return: 0
  */
 
@@ -11,13 +11,11 @@ void print_number(int n)
 	unsigned int n1;
 
 	n1 = n;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n1 = -n;
 	}
-
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
