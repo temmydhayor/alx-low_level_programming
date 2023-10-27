5d102c8650859e0bbd25bf3a58d4cbb884703e7d
temmydhayor <ttemitopealao@gmail.com>
2023-10-26 21:54:37 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,30 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * binary_to_uint - binary can be converted through unsigned int
- * @b: A string that takes binary number or numbers
- * Return: A numbers that are coverted
+ * @b: pointer to a string containing a binary number
+ *
+ * Return: unsigned int with decimal value of binary number, or 0 if error
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	int alx;
-	unsigned int temidayo = 0;
+	int a;
+	unsigned int num;
 
+	num = 0
 	if (!b)
-	return (0);
-	for (alx = 0; b[alx]; alx++)
+		return (0);
+	for (a = 0; b[a] != '\0'; a++)
 	{
-	if (b[alx] < '0' || b[alx] > '1')
-	return (0);
-	temidayo = 2 * temidayo + (b[alx] - '0');
+	if (b[a] != '0' && b[a] != '1')
+			return (0);
 	}
-	return (temidayo);
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		num <<= 1;
+		if (b[a] == '1')
+			num += 1;
+	}
+	return (num);
 }
